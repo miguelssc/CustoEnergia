c3a4ddce94a1e1c9fc805c902b4a27af07e6b821
miguel <miguelpositivo02@gmail.com>
2023-05-16 22:37:55 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("digite o tempo de uso diario");
+decimal diario =Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite o consumo do aparelho");
+decimal aparelho =Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("digite o tempo de uso diario");
+decimal custo =Convert.ToDecimal(Console.ReadLine());
+decimal total =(diario * aparelho * aparelho);
+Console.WriteLine($"{diario} * {aparelho} * {custo} = {total}");
+
+
+
+
